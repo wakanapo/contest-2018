5c3eb8567f871349a28180954d46d89840acba66
wakanapo <wakana.tn16@gmail.com>
2018-03-30 14:38:22 +0900

    簡単なのだいたいやった

--- gomoku.py
+++ gomoku.py
@@ -5,10 +5,25 @@ N = 15
 # The main routine of AI.
 # input: str[N][N] field : state of the field.
 # output: int[2] : where to put a stone in this turn.
+
+def LongestPoint(field, position, mark):
+    dists = [CountStonesOnLine(field, position, (1, 1), mark),
+     CountStonesOnLine(field, position, (1, 0), mark),
+     CountStonesOnLine(field, position, (1, -1), mark),
+     CountStonesOnLine(field, position, (0, 1), mark),
+     ]
+    return max(dists)
+
 def Think(field):
     CENTER = (int(N / 2), int(N / 2))
-
     best_position = (0, 0)
+    for i in range(N):
+        for j in range(N):
+            if field[i][j] != '.':
+                continue
+            if GetDistance((i, j), CENTER) < GetDistance(best_position, CENTER):
+                best_position = (i, j)
+
     for i in range(N):
         for j in range(N):
             if field[i][j] != '.':
@@ -17,32 +32,86 @@ def Think(field):
             position = (i, j)
             # Assume to put a stone on (i, j).
             field[i][j] = 'O'
-            if CanHaveFiveStones(field, position):
+            if CanHaveFiveStones(field, position, 'O'):
                 DebugPrint('I have a winning choice at (%d, %d)' % (i, j))
                 return position
+
+            field[i][j] = 'X'
+            if CanHaveFiveStones(field, position, 'X'):
+                return position
+
+            field[i][j] = 'X'
+            if CanHaveNStones(field, position, 'X', 4):
+                return position
+            
             # Revert the assumption.
             field[i][j] = '.'
-            if GetDistance(best_position, CENTER) > GetDistance(position, CENTER):
+            if LongestPoint(field, position, 'O') > LongestPoint(field, best_position, 'O'):
                 best_position = position
+
+            if LongestPoint(field, position, 'O') == LongestPoint(field, best_position, 'O'):
+               if CanHaveNStones(field, position, 'O', LongestPoint(field, position, 'O')):
+                   best_position = position
     return best_position
+
+def RangeCheck(position):
+    if position[0] > 14 or position[0] < 0:
+        return False
+    if position[1] > 14 or position[0] < 0:
+        return False
+    return True
+
+def CanHaveNStones(field, position, mark, n):
+    if CountStonesOnLine(field, position, (1, 1), mark) >= n:
+        if RangeCheck((position[0] + 1, position[1] + 1)) and field[position[0] + 1][position[1] + 1] == mark:
+            if RangeCheck((position[0] + n, position[1] + n)):
+                return field[position[0] + n][position[1] + n] == '.'
+            
+        if  RangeCheck((position[0] - 1, position[1] - 1)) and field[position[0] - 1][position[1] - 1] == mark:
+            if RangeCheck((position[0] - n, position[1] - n)):
+                return field[position[0] - n][position[1] - n] == '.'
+            
+    if CountStonesOnLine(field, position, (1, 0), mark) >= n:
+        if  RangeCheck((position[0] + 1, position[1])) and field[position[0] + 1][position[1]] == mark:
+            if RangeCheck((position[0] + n, position[1])):
+                return field[position[0] + n][position[1]] == '.'
+        if  RangeCheck((position[0] - 1, position[1])) and field[position[0] - 1][position[1]] == mark:
+            if  RangeCheck((position[0] - n, position[1])):
+                return field[position[0] - n][position[1]] == '.'
+            
+    if CountStonesOnLine(field, position, (1, -1), mark) >= n:
+        if  RangeCheck((position[0] + 1, position[1] - 1)) and field[position[0] + 1][position[1] - 1] == mark:
+            if  RangeCheck((position[0] + n, position[1] - n)):
+                return field[position[0] + n][position[1] - n] == '.'
+        if  RangeCheck((position[0] - 1, position[1] + 1)) and field[position[0] - 1][position[1] + 1] == mark:
+            if RangeCheck((position[0] - n, position[1] + n)):
+                return field[position[0] - n][position[1] + n] == '.'
+            
+    if CountStonesOnLine(field, position, (0, 1), mark) >= n:
+        if  RangeCheck((position[0], position[1] + 1)) and field[position[0]][position[1] + 1] == mark:
+            if  RangeCheck((position[0], position[1] + n)):
+                return field[position[0]][position[1] + n] == '.'
+        if  RangeCheck((position[0], position[1] - 1)) and field[position[0]][position[1] - 1] == mark:
+            if  RangeCheck((position[0], position[1] - n)):
+                return field[position[0]][position[1] - n] == '.'
 
 
 # Returns true if you have five stones from |position|. Returns false otherwise.
-def CanHaveFiveStones(field, position):
-    return (CountStonesOnLine(field, position, (1, 1)) >= 5 or
-            CountStonesOnLine(field, position, (1, 0)) >= 5 or
-            CountStonesOnLine(field, position, (1, -1)) >= 5 or
-            CountStonesOnLine(field, position, (0, 1)) >= 5)
+def CanHaveFiveStones(field, position, mark):
+    return (CountStonesOnLine(field, position, (1, 1), mark) >= 5 or
+            CountStonesOnLine(field, position, (1, 0), mark) >= 5 or
+            CountStonesOnLine(field, position, (1, -1), mark) >= 5 or
+            CountStonesOnLine(field, position, (0, 1), mark) >= 5)
 
 
 # Returns the number of stones you can put around |position| in the direction specified by |diff|.
-def CountStonesOnLine(field, position, diff):
+def CountStonesOnLine(field, position, diff, mark):
     count = 0
 
     row = position[0]
     col = position[1]
     while True:
-        if row < 0 or col < 0 or row >= N or col >= N or field[row][col] != 'O':
+        if row < 0 or col < 0 or row >= N or col >= N or field[row][col] != mark:
             break
         row += diff[0]
         col += diff[1]
@@ -51,7 +120,7 @@ def CountStonesOnLine(field, position, diff):
     row = position[0] - diff[0]
     col = position[1] - diff[1]
     while True:
-        if row < 0 or col < 0 or row >= N or col >= N or field[row][col] != 'O':
+        if row < 0 or col < 0 or row >= N or col >= N or field[row][col] != mark:
             break
         row -= diff[0]
         col -= diff[1]
